84aefc355d8a5085b28c1f10769c9d0ec0062b77
ADM228 <msh.alexgoog@gmail.com>
2023-08-04 19:56:22 +0300

    Confident wankdows fix 2

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,8 @@
 #include "Tile.hpp"
 #include "ChrFont.hpp"
 
-
+#define INCBIN_PREFIX bin_
+#define INCBIN_STYLE INCBIN_STYLE_SNAKE
 #define INCBIN_SILENCE_BITCODE_WARNING 1    // You cannot imagine how little of a fuck i give
 #include "incbin.h"
 INCBIN(font, "tilesetUnicode.chr");
