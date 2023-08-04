8f6b84d1540c6a9ccabedd51303564c621576db9
ADM228 <msh.alexgoog@gmail.com>
2023-08-04 18:40:52 +0300

    Windows fix (finally understood the issue)

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,10 @@
 #include "Tile.hpp"
 #include "ChrFont.hpp"
 
-#define INCBIN_PREFIX bin_
-#define INCBIN_STYLE INCBIN_STYLE_SNAKE
+
 #define INCBIN_SILENCE_BITCODE_WARNING 1    // You cannot imagine how little of a fuck i give
 #include "incbin.h"
 INCBIN(font, "tilesetUnicode.chr");
-
 
 
 int main()
