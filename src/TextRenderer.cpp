caa1f7ecdf0ed39d4346478cd67a3ddcaaf2b545
ADM228 <msh.alexgoog@gmail.com>
2023-08-26 01:31:08 +0300

    Halfwidth and fullwidth symbol interpretation

--- src/TextRenderer.cpp
+++ src/TextRenderer.cpp
@@ -12,6 +12,22 @@ class TextRenderer {
         static TileMatrix render (std::u32string text, int maxChars = -1, bool preprocess = 1);
     private:
         TextRenderer() {};
+        constexpr static uint16_t halfKatakanaTable[] {
+                                                                    0x2985, // U+FF5F                             ｟ 
+            0x2986, 0x3002, 0x300C, 0x300D, 0x3001, 0x30FB, 0x30F2, 0x30A1, // U+FF60	｠  ｡   ｢   ｣   ､   ･   ｦ   ｧ 
+            0x30A3, 0x30A5, 0x30A7, 0x30A9, 0x30E3, 0x30E5, 0x30E7, 0x30C3, // U+FF68   ｨ 	ｩ 	ｪ 	ｫ 	ｬ 	ｭ 	ｮ 	ｯ 
+            0x30FC, 0x30A2, 0x30A4, 0x30A6, 0x30A8, 0x30AA, 0x30AB, 0x30AD, // U+FF70	ｰ 	ｱ 	ｲ 	ｳ 	ｴ 	ｵ 	ｶ 	ｷ 
+            0x30AF, 0x30B1, 0x30B3, 0x30B5, 0x30B7, 0x30B9, 0x30BB, 0x30BD, // U+FF78	ｸ 	ｹ 	ｺ 	ｻ 	ｼ 	ｽ 	ｾ 	ｿ
+            0x30BF, 0x30C1, 0x30C4, 0x30C6, 0x30C8, 0x30CA, 0x30CB, 0x30CC, // U+FF80 	ﾀ 	ﾁ 	ﾂ 	ﾃ 	ﾄ 	ﾅ 	ﾆ 	ﾇ 
+            0x30CD, 0x30CE, 0x30CF, 0x30D2, 0x30D5, 0x30D8, 0x30DB, 0x30DE, // U+FF88	ﾈ 	ﾉ 	ﾊ 	ﾋ 	ﾌ 	ﾍ 	ﾎ 	ﾏ
+            0x30DF, 0x30E0, 0x30E1, 0x30E2, 0x30E4, 0x30E6, 0x30E8, 0x30E9, // U+FF90 	ﾐ 	ﾑ 	ﾒ 	ﾓ 	ﾔ 	ﾕ 	ﾖ 	ﾗ 
+            0x30EA, 0x30EB, 0x30EC, 0x30ED, 0x30EF, 0x30F3, 0x309B, 0x309C, // U+FF98	ﾘ 	ﾙ 	ﾚ 	ﾛ 	ﾜ 	ﾝ 	ﾞ 	ﾟ
+            0x3164                                                          // U+FFA0 [HWHF]
+        };
+        constexpr static uint16_t modWidthSymbolTable[] {
+            0x00A2, 0x00A3, 0x00AC, 0x00AF, 0x00A6, 0x00A5, 0x20A9, 0x0000, // U+FFE0	￠  ￡  ￢  ￣  ￤  ￥  ￦ 
+            0x2502, 0x2190, 0x2191, 0x2192, 0x2193, 0x25A0, 0x25CB, 0x0000  // U+FFE8	￨ 	￩ 	￪ 	￫ 	￬ 	￭ 	￮ 
+        };
 };
 
 std::u32string TextRenderer::preprocess(std::u32string string){
@@ -38,6 +54,8 @@ std::u32string TextRenderer::preprocess(std::u32string string){
         else if (character == 0x2060 || character == 0xFEFF)
             // Zero width non breaking spaces
             output_text.push_back(0x2060);
+        
+        // Hiragana and Katakana blocks (3040-30FF)
         else if (character >= 0x304B && character <= 0x3062 && (character & 1) == 0 ||              // Most dakuten hiragana
         character >= 0x30AB && character <= 0x30C2 && (character & 1) == 0 ||                       // Most dakuten katakana
         character == 0x3065 || character == 0x3067 || character == 0x3069 || character == 0x309E || // づ, で, ど, ゞ
@@ -61,6 +79,19 @@ std::u32string TextRenderer::preprocess(std::u32string string){
         } else if (character == 0x3099 || character == 0x309A) 
             // Combining versions of ゛ and ゜
             output_text.push_back(character+2);
+
+        // Halfwidth and fullwidth forms block (FF00-FFEF)
+        else if (character >= 0xFF01 && character <= 0xFF5E)
+            // Fullwidth versions of ASCII
+            output_text.push_back(character-0xFEE0);
+        else if (character >= 0xFF5F && character <= 0xFFA0)
+            // Halfwidth Katakana + some punctuation + [HWHF]
+            output_text.push_back(halfKatakanaTable[character-0xFF5F]);
+        //TODO: Halfwidth hangul when i add hangul support
+        else if (character >= 0xFFE0 && character <= 0xFFEF)
+            // Some symbols
+            output_text.push_back(modWidthSymbolTable[character-0xFFE0]);
+        
         else
             output_text.push_back(character);
     }
