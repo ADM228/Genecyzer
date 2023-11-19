7c56a2585a39a6caee0f05eeb2939055839a44f9
ADM228 <msh.alexgoog@gmail.com>
2023-11-19 19:18:28 +0300

    ... holy fucking hell this is becoming a mess

--- src/RIFF.cpp
+++ src/RIFF.cpp
@@ -21,60 +21,96 @@ namespace RIFF {
 
     enum RIFFError : int {
 
-    //Error codes (pass to riff_errorToString()), value mapping may change in the future
+    //Error codes (pass to ERRORToString()), value mapping may change in the future
     //non critical
-    RIFF_ERROR_NONE = 0,    //no error
-    RIFF_ERROR_EOC,         //end of current chunk, when trying to read/seek beyond end of current chunk data
-    RIFF_ERROR_EOCL,        //end of chunk list, if you are already at the last chunk in the current list level, occures when trying to seek the next chunk
-    RIFF_ERROR_EXDAT,       //excess bytes at end of chunk list level, not critical, the rest is simply ignored (1-7 bytes inside list, otherwise a following chunk is expected - more at file level possible), should never occur
-    RIFF_ERROR_EOF,         //same as RIFF_ERROR_EOCL but on the file level
+    ERROR_NONE = 0,     //no error
+    ERROR_EOC,          //end of current chunk, when trying to read/seek beyond end of current chunk data
+    ERROR_EOCL,         //end of chunk list, if you are already at the last chunk in the current list level, occures when trying to seek the next chunk
+    ERROR_EXDAT,        //excess bytes at end of chunk list level, not critical, the rest is simply ignored (1-7 bytes inside list, otherwise a following chunk is expected - more at file level possible), should never occur
+    ERROR_EOF,          //same as ERROR_EOCL but on the file level
 
     //critical errors
-    RIFF_ERROR_CRITICAL = 0x80,  //first critical error code ( & RIFF_ERROR_CRITICAL is always critical error)
+    ERROR_CRITICAL = 0x80,  //first critical error code ( & ERROR_CRITICAL is always critical error)
 
-    RIFF_ERROR_ILLID,       //illegal ID, ID (or type) contains not printable or non ASCII characters or is wrong
-    RIFF_ERROR_ICSIZE,      //invalid chunk size value in chunk header, too small or value exceeds list level or file - indicates corruption or cut off file 
-    RIFF_ERROR_FILESIZE,    //unexpected end of RIFF file, indicates corruption (wrong chunk size field) or a cut off file or the passed size parameter was wrong (too small) upon opening
-    RIFF_ERROR_ACCESS,      //access error, indicating that the file is not accessible (permissions, invalid file handle, etc.)
+    ERROR_ILLID,        //illegal ID, ID (or type) contains not printable or non ASCII characters or is wrong
+    ERROR_ICSIZE,       //invalid chunk size value in chunk header, too small or value exceeds list level or file - indicates corruption or cut off file 
+    ERROR_FILESIZE,     //unexpected end of RIFF file, indicates corruption (wrong chunk size field) or a cut off file or the passed size parameter was wrong (too small) upon opening
+    ERROR_ACCESS,       //access error, indicating that the file is not accessible (permissions, invalid file handle, etc.)
 
-    RIFF_ERROR_INVALID_HANDLE,  //riff_handle is not set up or is NULL
+    ERROR_INVALID_HANDLE,  //riff_handle is not set up or is NULL
     
 };
 
+#pragma region RIFFChunkBase
+
 class RIFFChunkBase {
     public:
+        /**
+         * @brief Read chunk's entire data from pointer
+         * 
+         * @tparam T 
+         * @param ptr 
+         * @return int - error code
+         */
+        template <typename T> int readData (T * ptr) {return readData((uint8_t *)ptr);};
         virtual int readData (uint8_t * ptr);
+
+        template <typename T> int readHeader (T * ptr) {return readHeader((uint8_t *)ptr);};
+        virtual int readHeader (uint8_t * ptr);
+
         virtual std::vector<uint8_t> * exportData ();
+
+        template <typename T> void exportData (T * ptr) {exportData((uint8_t *)ptr);};
         virtual void exportData (uint8_t * outPtr);
+
         virtual size_t size ();
+        template <typename T> inline int setType (T * ptr);
+        inline void setSize(size_t size) {data.resize(size);};
     protected:
         char type[CHUNK_NAME_LEN+1];
         std::vector<uint8_t> data;
         inline bool invalidHeader ();
-        template <typename T> static inline bool invalidHeader (T * ptr);
+
+        template <typename T> static inline bool invalidHeader (T * ptr) { return invalidHeader((uint8_t *)ptr); };
+        static inline bool invalidHeader (char * ptr);
+        static inline bool invalidHeader (uint8_t * ptr);
 };
 
 bool RIFFChunkBase::invalidHeader () {
     for (char i : type) {if (i&0x80) return 1;} return 0;
 }
 
-template <typename T> bool RIFFChunkBase::invalidHeader (T * ptr) {
+bool RIFFChunkBase::invalidHeader (char * ptr) {
     for (uint_fast8_t i = 0; i < CHUNK_NAME_LEN; i++) {if (ptr[i]&0x80) return 1;} return 0;
 }
 
-int RIFFChunkBase::readData (uint8_t * ptr) {
-    std::copy_n(ptr, CHUNK_NAME_LEN, type);
-    if (invalidHeader()) return RIFF_ERROR_ILLID;
+bool RIFFChunkBase::invalidHeader (uint8_t * ptr) {
+    for (uint_fast8_t i = 0; i < CHUNK_NAME_LEN; i++) {if (ptr[i]&0x80) return 1;} return 0;
+}
 
-    char buffer[sizeof(uint32_t)+1];
+
+template <typename T>  inline int RIFFChunkBase::setType (T * ptr) {
+    if (invalidHeader(ptr)) return ERROR_ILLID;
+    std::copy_n(ptr, CHUNK_NAME_LEN, type);
+    return ERROR_NONE;
+}
+
+int RIFFChunkBase::readHeader (uint8_t * ptr) {
+    std::copy_n(ptr, CHUNK_NAME_LEN, type);
+    if (invalidHeader()) return ERROR_ILLID;
+
     ptr+=CHUNK_NAME_LEN;
-    std::copy_n(ptr, sizeof(uint32_t), buffer);
-    data = std::vector<uint8_t>(readUint32(buffer));
+    data = std::vector<uint8_t>(readUint32(ptr));
+
+    return ERROR_NONE;
+}
+
+int RIFFChunkBase::readData (uint8_t * ptr) {
 
     ptr+=sizeof(uint32_t);
     std::copy_n(ptr, data.size(), data.data());
     
-    return RIFF_ERROR_NONE;
+    return ERROR_NONE;
 }
 
 std::vector<uint8_t> * RIFFChunkBase::exportData() {
@@ -95,6 +131,10 @@ size_t RIFFChunkBase::size() {
     return data.size()+CHUNK_NAME_LEN+sizeof(uint32_t);
 }
 
+#pragma endregion
+
+#pragma region RIFFListChunk
+
 class RIFFListChunk : public RIFFChunkBase {
     public:
         int readData (uint8_t * ptr) override;
@@ -108,21 +148,19 @@ class RIFFListChunk : public RIFFChunkBase {
 
 int RIFFListChunk::readData (uint8_t * ptr) {
     std::copy_n(ptr, CHUNK_NAME_LEN, type);
-    if (invalidHeader()) return RIFF_ERROR_ILLID;
+    if (invalidHeader()) return ERROR_ILLID;
 
-    char buffer[sizeof(uint32_t)+1];
     ptr+=CHUNK_NAME_LEN;
-    std::copy_n(ptr, sizeof(uint32_t), buffer);
-    data = std::vector<uint8_t>(readUint32(buffer-CHUNK_NAME_LEN));
+    data = std::vector<uint8_t>(readUint32(ptr)-CHUNK_NAME_LEN);
 
     ptr+=sizeof(uint32_t);
     std::copy_n(ptr, CHUNK_NAME_LEN, subtype);
-    if (invalidHeader(subtype)) return RIFF_ERROR_ILLID;
+    if (invalidHeader(subtype)) return ERROR_ILLID;
 
     ptr+=CHUNK_NAME_LEN;
     std::copy_n(ptr, data.size(), data.data());
     
-    return RIFF_ERROR_NONE;
+    return ERROR_NONE;
 }
 
 std::vector<uint8_t> * RIFFListChunk::exportData() {
@@ -136,7 +174,7 @@ void RIFFListChunk::exportData(uint8_t * outPtr) {
     if (invalidHeader()) return;
     uint8_t * ptr = outPtr;
     std::copy_n(type, CHUNK_NAME_LEN, outPtr);
-    writeBytes((uint32_t)data.size(), outPtr+CHUNK_NAME_LEN);
+    writeBytes((uint32_t)data.size()-CHUNK_NAME_LEN, outPtr+CHUNK_NAME_LEN);
     std::copy_n(type, CHUNK_NAME_LEN, outPtr+CHUNK_NAME_LEN+sizeof(uint32_t));
     ptr += CHUNK_NAME_LEN+sizeof(uint32_t)+CHUNK_NAME_LEN;
     for (RIFFChunkBase & subchunk : subchunks) {
@@ -151,12 +189,16 @@ size_t RIFFListChunk::size() {
     return output;
 }
 
+#pragma endregion
+
+#pragma region RIFFFile
+
 struct RIFFPtr {
     RIFFChunkBase chunk;
     std::streampos ptr;
 };
 
-#define readExit(__s, __t) std::fstream::read(__s, __t); if (fail()) return RIFF_ERROR_ACCESS;
+#define readExit(__s, __t) std::fstream::read(__s, __t); if (fail()) return ERROR_ACCESS;
 
 class RIFFFile : public RIFFChunkBase, protected std::fstream {      // the entire RIFF file format is a RIFF chunk in itself
     public:
@@ -172,10 +214,10 @@ class RIFFFile : public RIFFChunkBase, protected std::fstream {      // the enti
 
         ~RIFFFile();
 
-        // Read the latest chunk in the file, returns RIFF_ERROR_EOF if no chunks exist after this
+        // Read the latest chunk in the file, returns ERROR_EOF if no chunks exist after this
         int readNextChunk();
 
-        // Skip chunks in the file, returns RIFF_ERROR_EOF if no chunks exist after this
+        // Skip chunks in the file, returns ERROR_EOF if no chunks exist after this
         int seekChunk(int offset = 1);
 
         // Writes latest chunk to the file if open
@@ -184,7 +226,7 @@ class RIFFFile : public RIFFChunkBase, protected std::fstream {      // the enti
         // Writes the entire file to the file if open
         void writeFile();
 
-        char type[5];
+        char type[CHUNK_NAME_LEN+1];
     private:
         int readHeader();
         int parseChunkHeader();
@@ -204,36 +246,35 @@ void RIFFFile::writeLatestChunk() {
     delete[] temp;
 }
 
+#pragma region RIFFFile::open
+
 int RIFFFile::open( const char* filename, std::ios_base::openmode mode) { 
     std::fstream::open(filename, mode | std::ios::binary); 
-    if (fail()) return RIFF_ERROR_ACCESS; 
+    if (fail()) return ERROR_ACCESS; 
     return readHeader();
 }
 
 int RIFFFile::open( const std::string& filename, std::ios_base::openmode mode) { 
     std::fstream::open(filename, mode | std::ios::binary); 
-    if (fail()) return RIFF_ERROR_ACCESS; 
+    if (fail()) return ERROR_ACCESS; 
     return readHeader(); 
 }
 
 int RIFFFile::open( const std::filesystem::path& filename, std::ios_base::openmode mode) { 
     std::fstream::open(filename, mode | std::ios::binary); 
-    if (fail()) return RIFF_ERROR_ACCESS; 
+    if (fail()) return ERROR_ACCESS; 
     return readHeader(); 
 }
 
-template <typename T>
-void printByteArray(T * ptr, size_t size) {
-    for (size_t i = 0; i < size; i++) printf("%02X ", ptr[i]);
-}
+#pragma endregion
+
+template <typename T> void printByteArray(T * ptr, size_t size) { for (size_t i = 0; i < size; i++) printf("%02X ", ptr[i]); }
 
 int RIFFFile::readHeader () {
     char headerBuffer[CHUNK_NAME_LEN+1];
     // Read first chunk, should be "RIFF"
     readExit(headerBuffer, CHUNK_NAME_LEN);
-    if (strncmp(headerBuffer, "RIFF", CHUNK_NAME_LEN) != 0) {
-        return RIFF_ERROR_INVALID_HANDLE;
-    }
+    if (strncmp(headerBuffer, "RIFF", CHUNK_NAME_LEN) != 0) return ERROR_INVALID_HANDLE;
     
     // Read file length, should not exceed file length
     readExit(headerBuffer, 4);
@@ -241,18 +282,18 @@ int RIFFFile::readHeader () {
     
     std::fstream::seekg(length+CHUNK_NAME_LEN+sizeof(uint32_t)-1);
     std::fstream::read(headerBuffer, 1);
-    if (fail() || length < 4) return RIFF_ERROR_EOF;    // If cannot possibly even read header
+    if (fail() || length < 4) return ERROR_EOF;    // If cannot possibly even read header
 
     // Read file type
     std::fstream::seekg(CHUNK_NAME_LEN+sizeof(uint32_t));
     readExit(type, 4);
     
-    return RIFF_ERROR_NONE;
+    return ERROR_NONE;
 }
 
 int RIFFFile::seekChunk (int offset) {
     if (offset >= 0 && chunkPtr+offset >= chunks.size()) {
-        for (size_t i = chunkPtr; i < chunks.size(); i++) {
+        for (size_t i = chunkPtr; i < chunkPtr+offset; i++) {
             if (i >= chunks.size()) chunks.push_back({
                 RIFFChunkBase(), std::fstream::tellg()
             });
@@ -262,21 +303,19 @@ int RIFFFile::seekChunk (int offset) {
     }
     // if offset >= 1 && offset+ptr >= chunks.size: parse chunk headers
     // else: just adjust ptr
-    return RIFF_ERROR_NONE;
+    return ERROR_NONE;
 }
 
 int RIFFFile::parseChunkHeader() {
-    readExit(type, 4);
-    if (invalidHeader()) return RIFF_ERROR_ILLID;
+    char buffer[CHUNK_NAME_LEN+sizeof(uint32_t)+1];
+    readExit(buffer, CHUNK_NAME_LEN+sizeof(uint32_t));
+    if (readUint32(buffer+CHUNK_NAME_LEN) >= length-std::fstream::tellg()+8) return ERROR_FILESIZE;
+    chunks[chunkPtr].chunk.readHeader(buffer);
 
-    char buffer[sizeof(uint32_t)+1];
-    readExit(buffer, 4);
-    data = std::vector<uint8_t>(readUint32(buffer-CHUNK_NAME_LEN));
-
-    return RIFF_ERROR_NONE;
+    return ERROR_NONE;
 }
 
-
+#pragma endregion
 
 }
 
